7553b72e1a431c13f76b201da37a4f96aa64818e
Jacob Yarborough <jayarborough95@gmail.com>
2023-01-29 17:05:17 -0800

    Add capybara to gemfile and create songs feature test

---
+++ spec/features/songs/index_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe "songs index page", type: :feature do 
+  it "Can see all song titles and play count" do 
+    song1 = Song.create!(title: "I really like you", length: 208, play_count: 2468)
+    song2 = Song.create!(title: "Call me maybe", length: 199, play_count: 1357)
+
+    visit "/songs"
+
+    expect(page).to have_content(song1.title)
+    expect(page).to have_content("Play Count: #{song1.play_count}")
+    expect(page).to have_content(song2.title)
+    expect(page).to have_content("Play Count: #{song2.play_count}")
+  end 
+end 
