dfd0defa4fb7bbbfad1d5bf173681cda5f7b4310
Jacob Yarborough <jayarborough95@gmail.com>
2023-01-30 17:31:40 -0800

    Create artist and song realtionship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_31_011811) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_31_012949) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_31_011811) do
     t.integer "play_count"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "artist_id", null: false
+    t.index ["artist_id"], name: "index_songs_on_artist_id"
   end
 
+  add_foreign_key "songs", "artists"
 end
