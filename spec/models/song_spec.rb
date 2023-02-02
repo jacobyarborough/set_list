553174316fd978c41275540d24bb1bcf09c8995d
Jacob Yarborough <jayarborough95@gmail.com>
2023-02-01 18:22:31 -0800

    Create song_count method test

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -6,4 +6,17 @@ RSpec.describe Song, type: :model do
     it { should validate_presence_of :length }
     it { should validate_presence_of :play_count }
   end 
+
+  describe "class methods" do 
+    describe ".song_count" do 
+      it "returns the number of songs in the db" do 
+        prince = Artist.create!(name: 'Prince')
+        talking_heads = Artist.create!(name: 'Talking Heads')
+        rasperry_beret = prince.songs.create!(title: 'Raspberry Beret', length: 234, play_count: 34)
+        wild_life = talking_heads.songs.create!(title: 'Wild Wild Life', length: 456, play_count: 45)
+
+        expect(Artist.song_count).to eq(2)
+      end 
+    end 
+  end 
 end 
