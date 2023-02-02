5972be06373b59719868572333e247bb4dac94ee
Jacob Yarborough <jayarborough95@gmail.com>
2023-02-01 18:32:09 -0800

    Create average song length method for artist

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -8,4 +8,16 @@ RSpec.describe Artist, type: :model do
   describe "relationships" do 
     it { should have_many :songs }
   end 
+
+  describe "instance methods" do
+    describe "#average_song_length" do 
+      it "returns the average song length for a particular artist" do 
+        talking_heads = Artist.create!(name: 'Talking Heads')
+        she_was = talking_heads.songs.create!(title: 'And She Was', length: 234, play_count: 34)
+        wild_life = talking_heads.songs.create!(title: 'Wild Wild Life', length: 456, play_count: 45)
+  
+        expect(talking_heads.average_song_length).to eq(345)
+      end 
+    end 
+  end 
 end 
