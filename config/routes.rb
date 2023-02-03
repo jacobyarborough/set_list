0a296e4d8fdd412362fb8bd37e4b579aa1e2495b
Jacob Yarborough <jayarborough95@gmail.com>
2023-02-02 16:13:08 -0800

    Create root route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/", to: "welcome#index"
+
   get "/songs", to: "songs#index"
   get "/songs/:id", to: "songs#show"
 
