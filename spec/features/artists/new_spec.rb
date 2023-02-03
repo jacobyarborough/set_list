eca0e00ff51f9247fc2e793a78f4e1cdb9eb4a1b
Jacob Yarborough <jayarborough95@gmail.com>
2023-02-02 16:59:32 -0800

    Create artist new page

---
+++ spec/features/artists/new_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper" 
+
+RSpec.describe "New Artist Page", type: :feature do 
+  it "shows a form to create a new artist" do 
+    visit "/artists" 
+    click_link "Add New Artist"
+    expect(page).to have_current_path("/artists/new")
+
+    fill_in("Artist Name", with: "Beach Boys")
+    click_on("Create Artist")
+    expect(page).to have_current_path("/artists")
+    expect(page).to have_content("Beach Boys")
+  end 
+end 
