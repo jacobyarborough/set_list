a9d67f9b9a6359b000eff66c126f35ae88ad72cf
Jacob Yarborough <jayarborough95@gmail.com>
2023-02-02 21:19:30 -0800

    Refactor put to patch

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
   get "/artists/:id", to: "artists#show"
   get "artists/:id/edit", to: "artists#edit"
   post "/artists", to: "artists#create"
-  put "/artists/:id", to: "artists#update"
+  patch "/artists/:id", to: "artists#update"
   delete "/artists/:id", to: "artists#destroy"
 end
