53fec2ff48a2533b2e90de27fb8690a12594af3c
Jacob Yarborough <jayarborough95@gmail.com>
2023-02-05 20:35:30 -0800

    Create new method for songs controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -10,4 +10,18 @@ class SongsController < ApplicationController
   def new
     @artist = Artist.find(params[:id])
   end 
+
+  def create
+    song = Song.new(song_params)
+    song.artist_id = params[:id]
+    song.save!
+
+    redirect_to "/songs/#{song.id}"
+  end 
+
+  private
+
+  def song_params
+    params.permit(:title, :length, :play_count)
+  end 
 end 
